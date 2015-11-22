84f1e5bd1f04db60fbe53937d0c02ce586225608
Sergei Ledvanov <git@github.com>
2015-11-22 22:02:54 +0200

    - removed classLeakLoader plugin - cleaned up pom file - added resources config to app config

--- backend/src/main/java/com/sappadev/simplewebangular/conf/MvcConfig.java
+++ backend/src/main/java/com/sappadev/simplewebangular/conf/MvcConfig.java
@@ -2,14 +2,10 @@ package com.sappadev.simplewebangular.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
-@EnableWebMvc
 /**
  * @author (<a href="mailto:sergei.ledvanov@gmail.com">Sergei Ledvanov</a>) - 11/18/2015.
  */
@@ -18,6 +14,17 @@ public class MvcConfig extends WebMvcConfigurationSupport {
 	@Override
 	protected void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+				.addResourceLocations("/static/");
+	}
+
+	@Override
+	protected void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/index.html");
 	}
 
 	@Bean
--- backend/src/main/java/com/sappadev/simplewebangular/confservlet/ServletConfig.java
+++ backend/src/main/java/com/sappadev/simplewebangular/confservlet/ServletConfig.java
@@ -1,24 +1,13 @@
 package com.sappadev.simplewebangular.confservlet;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
 /**
+ * This should be empty because we do not have any servlet
+ * specific configuration in our app. Everything is initialized on
+ * the app level itself.
+ *
  * @author (<a href="mailto:sergei.ledvanov@gmail.com">Sergei Ledvanov</a>) - 22.11.2015.
  */
-public class ServletConfig extends WebMvcConfigurerAdapter {
+public class ServletConfig {
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**")
-				.addResourceLocations("/static/");
-	}
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addRedirectViewController("/", "/index.html");
-	}
 }
